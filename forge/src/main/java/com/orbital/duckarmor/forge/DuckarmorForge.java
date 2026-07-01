0b208044d7fd55d3ee2b15c7290a1264879d93e8
Orb4t3l <trevducket@gmail.com>
2026-06-30 23:46:42 -0700

    more debugging cuz i cant figureo ut ts

--- forge/src/main/java/com/orbital/duckarmor/forge/DuckarmorForge.java
+++ forge/src/main/java/com/orbital/duckarmor/forge/DuckarmorForge.java
@@ -15,21 +15,46 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(DuckarmorCommon.MODID)
 public class DuckarmorForge {
 
+    private static final Logger LOGGER = LogManager.getLogger(DuckarmorCommon.MODID);
+
     public DuckarmorForge() {
+        LOGGER.info("DuckArmor: starting Forge init");
+
+        try {
+            EntityDataHelper.register(new ForgeEntityDataProvider());
+            LOGGER.info("DuckArmor: EntityDataHelper registered");
+        } catch (Exception e) {
+            LOGGER.error("DuckArmor: FAILED at EntityDataHelper.register", e);
+            throw e;
+        }
+
+        try {
+            ModItems.init();
+            LOGGER.info("DuckArmor: ModItems.init() done");
+        } catch (Exception e) {
+            LOGGER.error("DuckArmor: FAILED at ModItems.init()", e);
+            throw e;
+        }
+
+        try {
+            ModCreativeTabs.init();
+            LOGGER.info("DuckArmor: ModCreativeTabs.init() done");
+        } catch (Exception e) {
+            LOGGER.error("DuckArmor: FAILED at ModCreativeTabs.init()", e);
+            throw e;
+        }
+
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        EntityDataHelper.register(new ForgeEntityDataProvider());
-        DuckarmorCommon.init();
-
-        // Game events go on the FORGE bus (entity/world events)
         MinecraftForge.EVENT_BUS.addListener(ForgeEvents::onLivingHurt);
         MinecraftForge.EVENT_BUS.addListener(ForgeEvents::onPlayerInteractEntity);
 
-        // BuildCreativeModeTabContentsEvent fires on the MOD bus, not the FORGE bus
         modBus.addListener((BuildCreativeModeTabContentsEvent event) -> {
             if (event.getTab() == ModCreativeTabs.DUCK_ARMOR_TAB) {
                 event.accept(new ItemStack(ModItems.DUCK_ARMOR.get()));
@@ -40,5 +65,7 @@ public class DuckarmorForge {
         if (FMLEnvironment.dist == Dist.CLIENT) {
             modBus.addListener(ForgeClientEvents::onAddEntityRenderLayers);
         }
+
+        LOGGER.info("DuckArmor: Forge init complete");
     }
 }
