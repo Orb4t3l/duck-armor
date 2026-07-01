8d6fc9eb9184244e05c4694b4b3a7ab8b66c0544
Orb4t3l <trevducket@gmail.com>
2026-06-30 23:40:21 -0700

    fixed issues

--- common/src/main/java/com/orbital/duckarmor/platform/Services.java
+++ common/src/main/java/com/orbital/duckarmor/platform/Services.java
@@ -1,21 +1,6 @@
 package com.orbital.duckarmor.platform;
 
-import dev.architectury.injectables.annotations.ExpectPlatform;
-
 public class Services {
-
-    @ExpectPlatform
-    public static void registerGameEvents() {
-        throw new AssertionError();
-    }
-
-    @ExpectPlatform
-    public static void registerClientEvents() {
-        throw new AssertionError();
-    }
-
-    @ExpectPlatform
-    public static void populateCreativeTab() {
-        throw new AssertionError();
-    }
+    public static void registerGameEvents() {}
+    public static void registerClientEvents() {}
 }
